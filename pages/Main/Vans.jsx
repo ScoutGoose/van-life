843ce05d6f7e0dc2b00dfbaa0e1b4271990125ba
victorlistapad <victorlistapad@gmail.com>
2025-07-16 01:30:03 -0400

    New: - Created van filter using search params - Created styles for filter buttons

--- pages/Main/Vans.jsx
+++ pages/Main/Vans.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
+import { useSearchParams, NavLink, Link } from "react-router-dom";
 import "../../styles/main/vans.css";
 import VanCard from "../../components/main-components/VanCard";
 import Loader from "../../components/main-components/Loader";
 export default function Vans() {
   const [vans, setVans] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
@@ -13,10 +16,48 @@ export default function Vans() {
   return (
     <section className="vans-page">
       <h2>Explore our van options</h2>
-      <div className="fiter-menu">{/* TODO */}</div>
+      <div className="filter-menu">
+        <NavLink
+          to="?type=simple"
+          className={({ isActive }) =>
+            isActive && typeFilter === "simple"
+              ? "active-filter simple"
+              : "simple"
+          }
+        >
+          Simple
+        </NavLink>
+        <NavLink
+          to="?type=luxury"
+          className={({ isActive }) =>
+            isActive && typeFilter === "luxury"
+              ? "active-filter luxury"
+              : "luxury"
+          }
+        >
+          Luxury
+        </NavLink>
+        <NavLink
+          to="?type=rugged"
+          className={({ isActive }) =>
+            isActive && typeFilter === "rugged"
+              ? "active-filter rugged"
+              : "rugged"
+          }
+        >
+          Rugged
+        </NavLink>
+        <Link to=".">Clear</Link>
+      </div>
       <div className="vans-options-container">
         {vans ? (
-          vans.map((van) => <VanCard {...van} key={van.id} />)
+          typeFilter ? (
+            vans
+              .filter((van) => van.type === typeFilter)
+              .map((van) => <VanCard {...van} key={van.id} />)
+          ) : (
+            vans.map((van) => <VanCard {...van} key={van.id} />)
+          )
         ) : (
           <Loader />
         )}
